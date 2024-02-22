b0d2406d4853b1c6ae22616309d4f5af96900805
arpstha <shrestharp88@gmail.com>
2024-02-22 16:26:31 +0000

    task 9 delete comments by comment_id completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -451,7 +451,7 @@ describe('PATCH /api/articles/:article_id', () => {
 
 }); 
 
-describe.only('DELETE /api/comments/:comment_id', () => {
+describe('DELETE /api/comments/:comment_id', () => {
     test('should return status 204 with message No Content', () => {
         return request(app)
         .delete('/api/comments/3')
